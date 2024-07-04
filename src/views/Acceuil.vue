4dcc37d66ad696b2c0140a6c89a8211af7eb74bc
Hospice Ahouandjinou <hospicesegnongiovanni@gmail.com>
2024-07-04 19:05:10 +0200

    En cours

--- src/views/Acceuil.vue
+++ src/views/Acceuil.vue
@@ -4,8 +4,32 @@
 
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <nav>
+      <h1>Nom de l'entreprise</h1>
+    </nav>
   </div>
+  
+  <div>
+    <div>
+     <form>
+      <label>Nom</label>
+        <input type="text" name="name" placeholder="____________">
+      </form>
+     <form>
+      <label>Email</label>
+        <input type="text">
+      </form>
+     <form>
+      <label>Contact</label>
+        <input type="text">
+      </form>
+    </div>
+
+    <div>
+      <form ></form>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
